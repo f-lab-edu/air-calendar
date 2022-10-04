4408085f9102f31c13dfeb0bd46287f6163b3072
herekim <khere220@gmail.com>
2022-10-04 17:43:07 +0900

    add: 헤더 클릭 시 캘린더 엘리먼트 append

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -1,15 +1,32 @@
 class MainCalendar extends HTMLElement {
   constructor() {
     super()
-    this.shadow = this.attachShadow({ mode: 'open' }) // shadowRoot를 반환
-  }
-
-  connectedCallback() {
+    this.attachShadow({ mode: 'open' }) // shadowRoot를 반환
     this.render()
   }
 
+  connectedCallback() {}
+
   render() {
-    this.shadow.innerHTML = `<p></p>` // shadowRoot에는 innerHTML, DOM method 사용 가능
+    /** Note
+     * shadowRoot에는 innerHTML, DOM method 사용 가능
+     */
+    this.shadowRoot.innerHTML = `
+    <style>
+      .calendar-wrapper {
+        position: fixed;
+        top: 100px;
+        left: 50%;
+        transform: translateX(-50%);
+        width: 80%;
+        height: 500px;
+        background-color: #DDDDDD;
+        border: 1px solid #DDDDDD;
+        border-radius: 20px;
+      }
+    </style>
+    <main class="calendar-wrapper"></main>
+    `
   }
 }
 
--- src/components/header.js
+++ src/components/header.js
@@ -1,39 +1,54 @@
 //Todo: template 분리
 const calendar = document.createElement('template')
 calendar.innerHTML = `
-  <div class="header-wrapper">
+  <header class="header-wrapper">
     <section class="header">
       <button>어디든지</button>
       <div class="divider"></div>
-      <button>언제든 일주일</button>
+      <button class="calendar-button">언제든 일주일</button>
       <div class="divider"></div>
       <button>게스트 추가</button>
       <button>🔎</button>
     </section>
-  </div>
+  </header>
 `
 
 class MainHeader extends HTMLElement {
   constructor() {
     super()
-    this.attachShadow({ mode: 'open' })
-    this.shadowRoot.append(calendar.content.cloneNode(true))
+    this.attachShadow({ mode: 'open' }).append(calendar.content.cloneNode(true))
+    this.render()
+    this.calendarOpen = false
+  }
 
-    // Note: link를 이용한 외부 스타일 시트의 성능
-    // 많은 모던 브라우저들은 공통 노드로부터 복제되었거나 동일한 텍스트를 가지고 있는
-    // <style> 태그에 대한 최적화를 구현하여 스타일 태그가 하나의 백업 스타일시트를 공유할 수 있게 합니다.
-    // 이 최적화로 인해 외부 스타일과 내부 스타일의 성능은 비슷할 것입니다.
+  handleClick() {
+    if (this.calendarOpen) {
+      const mainCalendar = document.querySelector('main-calendar')
+      mainCalendar.remove()
+      this.calendarOpen = false
+    } else {
+      const mainCalendar = document.createElement('main-calendar')
+      document.body.append(mainCalendar)
+      this.calendarOpen = true
+    }
+  }
+
+  connectedCallback() {
+    this.shadowRoot.querySelector('.calendar-button').addEventListener('click', this.handleClick.bind(this))
+  }
+
+  render() {
+    /**
+     * Note: link 태그를 통한 외부 스타일 시트의 성능 차이
+     * 많은 모던 브라우저들은 공통 노드로부터 복제되었거나 동일한 텍스트를 가지고 있는
+     * <style> 태그에 대한 최적화를 구현하여 스타일 태그가 하나의 백업 스타일시트를 공유할 수 있게 합니다.
+     * 이 최적화로 인해 외부 스타일과 내부 스타일의 성능은 비슷할 것입니다.
+     */
     const linkElem = document.createElement('link')
     linkElem.setAttribute('rel', 'stylesheet')
     linkElem.setAttribute('href', '../src/components/header.module.css')
     this.shadowRoot.append(linkElem)
   }
-
-  connectedRender() {
-    this.render()
-  }
-
-  render() {}
 }
 
 customElements.define('main-header', MainHeader)
