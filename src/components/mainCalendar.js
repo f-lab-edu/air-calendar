3de2e23a45729655fb367eaf2f4865c87a18cdb1
herekim <khere220@gmail.com>
2022-10-05 23:49:01 +0900

    add: 캘린더 현재 월, 다음 월 날짜 표시

--- src/components/mainCalendar.js
+++ src/components/mainCalendar.js
@@ -2,12 +2,14 @@ const calendar = document.createElement('template')
 calendar.innerHTML = `
   <main>
     <section class="main-calendar">
-      <article class="calendar-month">10월</article>
-      <article class="calendar-day"></article>
+      <article class="calendar-month"></article>
+      <article class="calendar-week"></article>
+      <article class="calendar-days"></article>
     </section>
     <secrion class="main-calendar">
-      <article class="calendar-month">11월</article>
-      <article class="calendar-day"></article>
+      <article class="next-calendar-month"></article>
+      <article class="calendar-week"></article>
+      <article class="next-calendar-days"></article>
     </secrion>
   </main>
 `
@@ -20,11 +22,59 @@ class MainCalendar extends HTMLElement {
     this.render()
   }
 
+  getYear(date) {
+    return date.getFullYear()
+  }
+
+  getMonth(date) {
+    return date.getMonth() + 1
+  }
+
+  getNumberOfDays(year, month) {
+    return new Date(year, month, 0).getDate()
+  }
+
+  getDayElements(numberOfDays) {
+    return Array.from(Array(numberOfDays).keys()).map((day) => day + 1)
+  }
+
+  getDayOfMonthInnerHTML(monthDates) {
+    return monthDates.map((date) => `<span class="calendar-day">${date}</span>`).join('')
+  }
+
+  getCalendar() {
+    const date = new Date()
+    const nextDate = new Date(date.getFullYear(), date.getMonth() + 1)
+
+    const calendarMonth = this.shadowRoot.querySelector('.calendar-month')
+    const calendarNextMonth = this.shadowRoot.querySelector('.next-calendar-month')
+
+    calendarMonth.textContent = `${this.getYear(date)}년 ${this.getMonth(date)}월`
+    calendarNextMonth.textContent = `${this.getYear(nextDate)}년 ${this.getMonth(nextDate)}월`
+
+    const calendarDay = this.shadowRoot.querySelector('.calendar-days')
+    const calendarNextDay = this.shadowRoot.querySelector('.next-calendar-days')
+
+    const numberOfDays = this.getNumberOfDays(this.getYear(date), this.getMonth(date))
+    const nextNumberOfDays = this.getNumberOfDays(this.getYear(nextDate), this.getMonth(nextDate))
+
+    const monthDates = this.getDayElements(numberOfDays)
+    const nextMonthDates = this.getDayElements(nextNumberOfDays)
+
+    const dayOfMonthInnerHTML = this.getDayOfMonthInnerHTML(monthDates)
+    const nextDayOfMonthInnerHTML = this.getDayOfMonthInnerHTML(nextMonthDates)
+
+    calendarDay.innerHTML = dayOfMonthInnerHTML
+    calendarNextDay.innerHTML = nextDayOfMonthInnerHTML
+  }
+
   render() {
     const linkElem = document.createElement('link')
     linkElem.setAttribute('rel', 'stylesheet')
     linkElem.setAttribute('href', '../src/components/mainCalendar.module.css')
     this.shadowRoot.append(linkElem)
+
+    this.getCalendar()
   }
 }
 
